52ab1d33abce3021b54799194ba7117d79b51d45
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-10-18 10:14:30 +0100

    Initial Commit

---
+++ FirebaseStarter/FirebaseStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseStarterApp.swift
+//  FirebaseStarter
+//
+//  Created by Jack Finnis on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
